54c801978e7e10929fa855a805d37dcecc090927
pratiksha parihar <pratikshaparihar679@gmail.com>
2026-02-13 22:32:46 +0530

    added env file

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://finance-portfolio-j579.onrender.com"
 })
 
 export const fetchPortfolio = () =>
